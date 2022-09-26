33de9b5b77d8d5412265a2ad2014d98c6795f785
CodeSocrates <furo9899@naver.com>
2022-09-27 00:40:10 +0900

    test: 라우팅 적용 전 테스트

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from "./pages/MainPage";
-import Payment from "./pages/Payment";
-import ProductDetail from "./pages/ProductDetail";
-import UserPage from "./pages/UserPage";
 
 function App() {
   return (
-    <BrowserRouter basename="ecommerce">
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/product/:productId" element={<ProductDetail />} />
-      <Route path="/Payment" element={<Payment />} />
-      <Route path="/UserPage" element={<UserPage />} />
-    </Routes>
-  </BrowserRouter>
-    /*<BrowserRouter basename="ecommerce">
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/product/:productId" element={<ProductDetail />} />
-      <Route path="/Payment" element={<Payment />} />
-      <Route path="/UserPage" element={<UserPage />} />
-    </Routes>
-  </BrowserRouter>*/
+    <div className="App">
+      리액트 기반의 이커머스
+    </div>
   );
 }
 
