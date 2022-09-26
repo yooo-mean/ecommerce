81a65dee727af65fe69a561e088f8238ab9d9904
CodeSocrates <furo9899@naver.com>
2022-09-27 01:53:47 +0900

    test: href 속성 eslint 오류 테스트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
         <a href="http://localhost:3000/ecommerce/product/1">마이페이지</a>
       </header>
       <aside>
-        <a href="http://localhost:3000/ecommerce/product/1">가전제품</a>
-        <a href="http://localhost:3000/ecommerce/product/1">컴퓨터</a>
-        <a href="http://localhost:3000/ecommerce/product/1">전자제품</a>
-        <a href="http://localhost:3000/ecommerce/product/1">남성의류</a>
+        <a href="http://localhost:3000/ecommerce/product/2">가전제품</a>
+        <a href="http://localhost:3000/ecommerce/product/3">컴퓨터</a>
+        <a href="http://localhost:3000/ecommerce/product/4">전자제품</a>
+        <a href="http://localhost:3000/ecommerce/product/5">남성의류</a>
         <a href="http://localhost:3000/ecommerce/basket">결제</a>
       </aside>
     </>
