f1034493c0727475f77bdfbb699cc6ec62e54e5c
CodeSocrates <furo9899@naver.com>
2022-09-26 23:55:27 +0900

    feat: 메인페이지 추가 및 앱파일 jsx로 확장자 변경

--- src/App.js
+++ src/App.js
@@ -7,23 +7,14 @@ import UserPage from "./pages/UserPage";
 
 function App() {
   return (
-  <div>
-    <header>
-      <span>유민몰</span>
-      <input></input>
-      <button>🔍</button>
-      <a>로그인|</a>
-      <a>회원가입|</a>
-      <a>마이페이지</a>
-    </header>
-    <aside>
-      <a href="http://localhost:3000/">가전제품</a>
-      <a href="http://localhost:3000/">컴퓨터</a>
-      <a href="http://localhost:3000/">전자제품</a>
-      <a href="http://localhost:3000/">남성의류</a>
-      <a href="http://localhost:3000/">여성의류</a>
-    </aside>
-  </div>
+    <BrowserRouter basename="ecommerce">
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/product/:productId" element={<ProductDetail />} />
+      <Route path="/Payment" element={<Payment />} />
+      <Route path="/UserPage" element={<UserPage />} />
+    </Routes>
+  </BrowserRouter>
     /*<BrowserRouter basename="ecommerce">
     <Routes>
       <Route path="/" element={<MainPage />} />
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,23 @@
 const MainPage = () => {
-  return <div>메인 페이지</div>;
+  return (
+    <div>
+      <header>
+        <span>유민몰</span>
+        <input></input>
+        <button>🔍</button>
+        <a>로그인|</a>
+        <a>회원가입|</a>
+        <a>마이페이지</a>
+      </header>
+      <aside>
+        <a href="http://localhost:3000/">가전제품</a>
+        <a href="http://localhost:3000/">컴퓨터</a>
+        <a href="http://localhost:3000/">전자제품</a>
+        <a href="http://localhost:3000/">남성의류</a>
+        <a href="http://localhost:3000/">여성의류</a>
+      </aside>
+    </div>
+  );
 };
 
 export default MainPage;
