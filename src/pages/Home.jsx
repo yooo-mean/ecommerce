914923e24937746699ccb7b419e7a423c1da457e
CodeSocrates <furo9899@naver.com>
2022-11-01 04:03:21 +0900

    refactor : DB 다루는 api 별도 분리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,59 +5,7 @@ import ProductCard from '../components/ProductCard';
 import styled from 'styled-components';
 import {mockTheme1Products, mockTheme2Products} from "../data/mockData"
 import { useState, useEffect } from 'react';
-import { db, firebase } from '../firebase';
-
-const getDoc = async (colId, docId) => {
-  try {
-    const doc = await db.collection(colId).doc(docId).get()
-    if(doc.exists) {
-      console.log(doc.data());
-    } else {
-      console.log('해당 document 없음!');
-    }
-    console.log('get : good!');
-  } catch(e) {
-    alert('get : err!');
-  }
-}
-
-const addDoc = async (colId, data) => {
-  try {
-    await db.collection(colId).add(data);
-    console.log('add : good!');
-  } catch(e) {
-    alert('add : err!');
-  }
-}
-
-const updateDoc = async (colId, docId, data) => {
-	try {
-		await db.collection(colId).doc(docId).update(data);
-		console.log('update : good!');
-	} catch(e) {
-		alert('update : err!');
-	}
-}
-
-const deleteDoc = async (colId, docId) => {
-  try {
-    await db.collection(colId).doc(docId).delete();
-    console.log('delete document : good!');
-  } catch(e) {
-    alert('delete document : err!');
-  }
-};
-
-const deleteField = async (colId, docId, fieldName) => {
-  try {
-    const data = {};
-    data[fieldName] = firebase.firestore.FieldValue.delete();
-    await db.collection(colId).doc(docId).update(data)
-    console.log('delete field : good!')
-  } catch(e) {
-    alert('delete field : err!');
-  }
-}
+import {getDoc, addDoc, updateDoc, deleteDoc, deleteField} from "../apis/useDB"
 
 const Home = () => {
   getDoc('board', 'Fy7wjB6oMtQz1rgb6QeB');
